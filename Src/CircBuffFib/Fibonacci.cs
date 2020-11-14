ec214fce01a66d8878951a6cf1c301536a6bcaf1
status203 <git@status203.com>
2020-11-14 17:45:55 +0000

    Implemented - tests pass

--- Src/CircBuffFib/Fibonacci.cs
+++ Src/CircBuffFib/Fibonacci.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections.Generic;
 
 namespace CircBuffFib
@@ -6,7 +5,25 @@ namespace CircBuffFib
     public static class Fibonacci
     {
         public static IEnumerable<int> Sequence(int first, int second) {
-            throw new NotImplementedException();
+            yield return first;
+            yield return second;
+
+            var buffer = new int[4];
+            var mask = 0_11;
+            buffer[0] = first;
+            buffer[1] = second;
+
+            var firstIndex = 0;
+
+            while(true) {
+                var index = firstIndex;
+                var total = buffer[index];
+                firstIndex = index = ++index & mask;
+                total += buffer[index];
+                index = ++index & mask;
+                buffer[index] = total;
+                yield return total;
+            }
         }
     }
 }
